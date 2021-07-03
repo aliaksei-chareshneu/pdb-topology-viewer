c8ed81e39813556e450d94d3b2c156b5f3a39f07
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2021-07-03 12:02:12 +0300

    Implement correct coords convertion (cartesian to y-reversed)

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,3 +1,8 @@
+// TODO: Convert cartesian to Y-reversed coordinates
+// TODO: Fix tsc errors
+// TODO: Copy updated function to index.ts and continue development of this function in index.ts
+// TODO: Implement rotation
+// TODO: Check if residues from some of 5 used APIs correspond to what is in 2DProts
 // TODO: Write better description
 // Function converts 2DProts JSON to "PDBe-topology-API-like" JSON suitable for drawing SSEs via modified PDB Topology Component
 function convert2DProtsJSONtoTopologyAPIJSON(inputJson, entryID, chainID) {
@@ -48,11 +53,36 @@ function convert2DProtsJSONtoTopologyAPIJSON(inputJson, entryID, chainID) {
 	const inputHelices = inputSSEs.filter(sse => sse[0].charAt(0) === 'H');
 	// console.log(inputHelices);
 	outputJSON[entryID]['1'][chainID].helices = inputHelices.map(helix => {
-		// console.log(helix);
+		console.log(helix);
 		const center = {
 			'x': helix[1].layout[0],
 			'y': helix[1].layout[1],
 		};
+		
+		const pathCartesian = [
+			center.x + (MINORAXIS/2), center.y + (helix[1].size/2) - CONVEXITY,
+			center.x,				  center.y + (helix[1].size/2),
+			center.x - (MINORAXIS/2), center.y + (helix[1].size/2) - CONVEXITY,
+			center.x - (MINORAXIS/2), center.y - (helix[1].size/2) + CONVEXITY,
+			center.x,				  center.y - (helix[1].size/2),
+			center.x + (MINORAXIS/2), center.y - (helix[1].size/2) + CONVEXITY
+		];
+		
+		const pathYReversed = pathCartesian.map((coord, index) => {
+			if (index % 2 === 0) {
+				return coord - lowerLeft.x;
+			} else {
+				return upperRight.y - coord;
+			}
+		});
+		
+		const centerYReversed = {
+			'x': center.x - lowerLeft.x,
+			'y': upperRight.y - center.y
+		};
+		
+		console.log(pathYReversed);
+		console.log(centerYReversed);
 		
 		return {
 			'start': Number(helix[1].residues[0]),
@@ -61,15 +91,10 @@ function convert2DProtsJSONtoTopologyAPIJSON(inputJson, entryID, chainID) {
 			'minoraxis': MINORAXIS,
 			// We can try 2 points here, and let the existing code of topology component convert it
 			// But better to let the code commented, and supply 6 points from the very beginning
-			'path': [
-				center.x + (MINORAXIS/2), center.y + (helix[1].size/2) - CONVEXITY,
-				center.x,				  center.y + (helix[1].size/2),
-				center.x - (MINORAXIS/2), center.y + (helix[1].size/2) - CONVEXITY,
-				center.x - (MINORAXIS/2), center.y - (helix[1].size/2) + CONVEXITY,
-				center.x,				  center.y - (helix[1].size/2),
-				center.x + (MINORAXIS/2), center.y - (helix[1].size/2) + CONVEXITY
-			],
+			'path': pathYReversed,
+			'center': centerYReversed,
 			'color': helix[1].color,
+			'angle': helix[1].angles,
 			'2dprotsSSEId': helix[0],
 		}
 	});
@@ -988,6 +1013,12 @@ class PdbTopologyViewerPlugin {
                             // //bring original/complete helices in front
                             // angular.element(element[0].querySelector('.topoSvg')).append(newEle.node());
                             this.svgEle._groups[0][0].append(newEle.node());
+							
+							const xCenterScaled = this.xScale(secStrData.center.x);
+							const yCenterScaled = this.yScale(secStrData.center.y);
+							const allElementsBelongingToHelix = d3.selectAll(`.helices${secStrDataIndex}, .maskpath-helices${secStrDataIndex}, .subpath-helices${secStrDataIndex}`)
+								.attr('transform', `rotate(${secStrData.angle}, ${xCenterScaled}, ${yCenterScaled})`);
+							console.log(allElementsBelongingToHelix);
                         }
                     
                         //for coils
